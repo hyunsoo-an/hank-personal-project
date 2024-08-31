178b5b0a27a160ea8e41fe837fa9d9e7dfff7ebd
Hyunsoo An <hankjudolove@gmail.com>
2024-08-31 23:17:17 +1200

    created seed and models interface

--- server/db/fruits.ts
+++ server/db/fruits.ts
@@ -1,6 +1,6 @@
 import connection from './connection.ts'
-import { Fruit } from '../../models/fruit.ts'
+import { Pet } from '../../models/pet.ts'
 
-export async function getAllFruits(db = connection): Promise<Fruit[]> {
-  return db('fruit').select()
+export async function getAllPets(db = connection): Promise<Pet[]> {
+  return db('pet').select()
 }
